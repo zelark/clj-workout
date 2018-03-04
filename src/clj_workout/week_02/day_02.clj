67b170d66e70d82e0de68ce9cc76b941489f6101
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-04 08:37:52 +0300

    Week 02: day 02

---
+++ src/clj_workout/week_02/day_02.clj
@@ -0,0 +1,52 @@
+(ns clj-workout.week-02.day-02)
+
+
+;; Advanced Destructuring
+;;
+;; Here is an example of some more sophisticated destructuring.
+(= [1 2 [3 4 5] [1 2 3 4 5]]
+   (let [[a b & c :as d] [1 2 3 4 5]]
+     [a b c d]))
+
+
+;; A Half-Truth
+;; 
+;; Write a function which takes a variable number of booleans.
+;; Your function should return true if some of the parameters are true,
+;; but not all of the parameters are true. Otherwise your function
+;; should return false.
+(defn half-true? [& xs]
+  (if (and (not-every? true? xs)
+           (some true? xs))
+    true
+    false))
+
+(= false (half-true? false false))
+
+(= true (half-true? true false))
+
+(= false (half-true? true))
+
+(= true (half-true? false true false))
+
+(= false (half-true? true true true))
+
+(= true (half-true? true true true false))
+
+
+;; Greatest Common Divisor
+;;
+;; Given two integers, write a function which returns
+;; the greatest common divisor.
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(= (gcd 2 4) 2)
+
+(= (gcd 10 5) 5)
+
+(= (gcd 5 7) 1)
+
+(= (gcd 858 1023) 33)
